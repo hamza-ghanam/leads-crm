9ec4caf11ca4150e63982dd76abd66d1f1881c16
Hamza Ghanam <hamza.ghanam.92@gmail.com>
2025-12-21 23:42:27 +0400

    Add FCM Token

---
+++ app/Models/FcmToken.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class FcmToken extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['user_id', 'token', 'device_type', 'user_agent', 'last_used_at'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
